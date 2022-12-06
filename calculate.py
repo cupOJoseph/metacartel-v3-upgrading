0d5ddd6f1a84aba208fe5b19fa100490af67b849
Joseph Schiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2022-12-06 17:51:13 -0500

    remove extra cols

--- calculate.py
+++ calculate.py
@@ -9,5 +9,8 @@ df["square rooted shares"] = df["shares"].apply(lambda x: x ** 0.5)
 # Create a new column for the difference between the original shares and the square rooted shares
 df["difference"] = df["shares"] - df["square rooted shares"]
 
+# Drop the unnecessary columns. The final result must be the format required for generating the moloch v3 dao
+df = df.drop(columns=["kicked", "jailed", "tokenTribute", "didRagequit", "exists", "createdAt", "isDao", "isSafeMinion"])
+
 # Save the DataFrame to a new CSV file
 df.to_csv("output.csv", index=False)

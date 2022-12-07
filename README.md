1c88d20685e11894554fb3ebad20351d14c79289
jschiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2022-12-06 21:32:54 -0500

    output result

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # metacartel-quadratic-upgrading
+
 Upgrade the meta cartel DAO to moloch v3 quadratically
+
+Creating a new DAO we can use the Moloch v3 output csv:
+
+- "square rooted shares" become shares
+- "difference" becomes loot
--- calculate.py
+++ calculate.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv("input.csv")
+df = pd.read_csv("metacartel-v2-hd_Members_1667319488.csv")
 
 # Create a new column for the square rooted shares
 df["square rooted shares"] = df["shares"].apply(lambda x: x ** 0.5)
@@ -10,7 +10,7 @@ df["square rooted shares"] = df["shares"].apply(lambda x: x ** 0.5)
 df["difference"] = df["shares"] - df["square rooted shares"]
 
 # Drop the unnecessary columns. The final result must be the format required for generating the moloch v3 dao
-df = df.drop(columns=["kicked", "jailed", "tokenTribute", "didRagequit", "exists", "createdAt", "isDao", "isSafeMinion"])
+df = df.drop(columns=["kicked", "jailed", "tokenTribute", "didRagequit", "exists", "createdAt", "isDao", "isSafeMinion", "delegateKey", "shares", "loot"])
 
 # Save the DataFrame to a new CSV file
-df.to_csv("output.csv", index=False)
+df.to_csv("moloch-v3-output.csv", index=False)
